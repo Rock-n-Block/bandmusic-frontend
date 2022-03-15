45e138e60e72470cd5458f78f2dfa51cdd76e751
andrey <mastercatpro.tm@gmail.com>
2022-03-15 12:18:37 +0300

    add redirect after disconnect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import { observer } from 'mobx-react-lite';
 
 import { AdminModal, Header, RouterManager } from '@/containers';
 
+import { useMst } from './store';
 
 const App: FC = observer(() => {
+  const { address } = useMst().user;
+  const navigator = useNavigate();
+  useEffect(() => {
+    if (address.length === 0) {
+      navigator('/');
+    }
+  }, [address.length, navigator]);
   return (
     <>
       <AdminModal />
